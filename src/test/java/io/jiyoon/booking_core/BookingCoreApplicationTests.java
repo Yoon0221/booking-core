656168088a60c9fc47cd994bfc20ef392f47674a
yoon <sin020511@gmail.com>
2026-05-21 21:34:40 +0900

    chore: initialize spring boot project

---
+++ src/test/java/io/jiyoon/booking_core/BookingCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jiyoon.booking_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
