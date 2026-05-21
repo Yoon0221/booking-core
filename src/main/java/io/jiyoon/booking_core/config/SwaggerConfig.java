7235349322a8d0d9574b7c177c6e70945d0edae3
yoon <sin020511@gmail.com>
2026-05-21 22:01:07 +0900

    chore: add springdoc-openapi swagger configuration

---
+++ src/main/java/io/jiyoon/booking_core/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package io.jiyoon.booking_core.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Booking Core API")
+                        .description("Reservation & Checkout System API Documentation")
+                        .version("1.0.0"));
+    }
+}
